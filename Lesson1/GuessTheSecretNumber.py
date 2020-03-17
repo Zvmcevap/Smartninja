fd744518b9e8157b64d6394d9d64921e1f120641
Zvmcevap <60576794+Zvmcevap@users.noreply.github.com>
2020-03-17 18:26:52 +0100

    Add files via upload

--- Lesson1/GuessTheSecretNumber.py
+++ Lesson1/GuessTheSecretNumber.py
@@ -1,14 +1,24 @@
-# Variables
-secret = 7
 
-won = "Congrats!!!!"
-lost = "Loser!"
+import  random
+
+# Variables
+secret = random.randint(1, 10)
+
+won = "Congrats!!!! "
+lost = "Loser! "
 
 # Fun part
 
-guess = int(input("Guess the secret number: "))
-
-if guess == secret:
-    print(won)
-else:
-    print(lost)
+while True:
+    guess = int(input("Guess the secret number: "))
+    if guess == secret:
+        print(won)
+        break
+    elif guess == 8:
+        print("Warmer...warmerr... ")
+    elif guess == 6:
+        print("So freakin CLOSE! ")
+    elif guess < secret:
+        print(lost + "Try a bigger number. ")
+    elif guess > secret:
+        print(lost + "Try a smaller number.")
