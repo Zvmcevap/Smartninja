867d4a1b41177717e2e35fb7d874084ff18bea8f
Zvmcevap <60576794+Zvmcevap@users.noreply.github.com>
2020-03-17 17:56:31 +0100

    Add files via upload

---
+++ Lesson2/MoodChecker.py
@@ -0,0 +1,33 @@
+# Variables
+
+mood = input("Please enter your mood: ")
+
+some_string = "Hey there! "
+another_string = "Another hey! "
+
+# if and if not and if else
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif mood == "nervous":
+    print("Calm the fuck down!")
+
+elif mood == "sad":
+    print("Boo hoo for you! :(((")
+
+elif mood == "excited":
+    print("WOOOP!!!!!!!")
+
+elif mood == "relaxed":
+    print("As a Bob Marley after a reefer")
+else:
+    print("Either thats not a mood or you didnt use all lowercase letters! Also avoid punctuation sigh")
+
+bool_one = True
+bool_two = False
+
+some_var = None
+
+# Operations
+
+print(some_string + another_string)
