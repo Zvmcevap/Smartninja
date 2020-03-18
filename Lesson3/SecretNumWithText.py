e2bc13fbc311bdcf02bb1c7477cba4af63a091f2
Zvmcevap <60576794+Zvmcevap@users.noreply.github.com>
2020-03-18 18:47:36 +0100

    Update SecretNumWithText.py

--- Lesson3/SecretNumWithText.py
+++ Lesson3/SecretNumWithText.py
@@ -1,10 +1,11 @@
 import random
-
+# Predefined variables to not break Python
 best_score = ""
 last_attempt = ""
+only = ""
+rev_last_attempt = ""
 
-# Text document stuff
-
+# Open text and check whats useful in it
 with open("score.txt", "r") as score_file:
     lines = score_file.readlines()
     n = len(lines) - 1
@@ -12,12 +13,21 @@ with open("score.txt", "r") as score_file:
         for i in lines[0]:
             if i.isdigit():
                 best_score += i
-        for i in lines[n]:
-            if i.isdigit():
-                last_attempt += i
+        for i in reversed(lines[n]):
+            if i == ":":
+                break
+            elif i.isdigit():
+                rev_last_attempt += i
+    else:
+        n = 0
+        lines = [None]
 
+# Get the stats from the last game if any available
 try:
+    for i in reversed(rev_last_attempt):
+        last_attempt += i
     last_attempt = int(last_attempt)
+    print(f"Game {n + 1}! GO!")
 except:
     last_attempt = ""
 
@@ -26,21 +36,23 @@ try:
 except:
     best_score = 1000
 
+# Secret number generation
 secret = random.randint(1, 30)
-print(secret)
+print(f"Its: {secret}")
 attempts = 0
 
+# Fun play time (put in system to charge per attempt later)
 while True:
     try:
+        print(f"Attempt number: {attempts + 1}!")
         guess = int(input("Guess the secret number between 1-30: "))
         attempts += 1
-        print(attempts)
 
         if guess > 30 or guess < 1:
             print("Please stay within 1 and 30. ")
             continue
     except:
-        print("Please write a NUMBER between 1-30: ")
+        print("Please write a valid NUMBER! ")
         attempts += 1
         print(f"This still counted as an attempt number {attempts}! ")
 
@@ -51,14 +63,20 @@ while True:
         elif guess > secret:
             print("Try a smallah numbah!")
 
+# Win state (and eventual credit card charges (WIN STATE HUZZAH!))
         else:
             last_attempt = attempts
+            if attempts < 5:
+                only = "only "
             if best_score > attempts:
                 best_score = attempts
+
+            lines[0] = f"Best score: {best_score} tries. \n"
+            lines.append(f"Game number {n + 1}: {last_attempt} tries. \n")
             with open("score.txt", "w") as score_file:
-                score_file.write(f"Best score: {best_score}\n")
-                score_file.write(f"Last score: {last_attempt}\n")
+                for x in lines:
+                    score_file.write(x)
 
             print(f"Congratulations,youve guessed the secret number {secret}! ")
-            print(f"And you only needed {attempts} attempts! ")
+            print(f"And you {only}needed {attempts} attempts! ")
             break
