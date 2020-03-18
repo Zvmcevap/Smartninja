4f2a27412cf5fa0d677bf916ccc8645b5d144c37
Zvmcevap <60576794+Zvmcevap@users.noreply.github.com>
2020-03-18 16:43:23 +0100

    Update UnitConverter.py

--- Lesson2/UnitConverter.py
+++ Lesson2/UnitConverter.py
@@ -16,16 +16,16 @@ km_total = 0.0
 #Beeps and Boops here
 
 if gender == "Male":
-        print("Hello, " + "Mr. " + user_name)
+    print("Hello, " + "Mr. " + user_name)
 elif gender == "Female":
-        print("Hello, " + "Ms. " + user_name)
+    print("Hello, " + "Ms. " + user_name)
 
 while cont_string == "y":
     km = float(input("Enter kilometers: "))
     km_total = km_total + km
     print(km * conv_rate, "miles")
     print(km_total, "km talley.")
-    cont_string = (input("Type y to continue: "))
+    cont_string = input("Type y to continue: ")
 
 print(user_name + ", thank you for using UnitConverter(tm). Your Credit Card has been charged 99c per unit of km converted as per User License Agreenment. Which amounts to ",price_rate * km_total,"€ total.")
 print("Thank you for using UnitConverted(tm) patent pendint. Have a fantastic day!")
